11652eccbdaed916e59049e87f894f4c5925689b
Chris Grimmett <chris@grimtech.net>
2018-09-07 18:38:44 -0700

    add assetspublicpath

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/ld42/',
 
     /**
      * Source Maps
